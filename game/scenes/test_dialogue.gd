0049248e2703e1f2be1f92e1177b59811d76f9a1
Chen-Xiang-Ji <a841003@gmail.com>
2025-08-03 14:36:10 +0800

    chore: 拿掉測試場景

--- game/scenes/test_dialogue.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-@export var test_dialogue:DialogueResource 
-
-func _ready():
-	var dialogue_line = await DialogueManager.get_next_dialogue_line(test_dialogue, "start")
-	DialogueManager.show_dialogue_balloon(test_dialogue)
