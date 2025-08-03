ae88692a7c2cdda31c603eecba257156ea9ec239
Chen-Xiang-Ji <a841003@gmail.com>
2025-08-03 15:23:39 +0800

    feat: 加入木匠角色場景

--- game/helpers/actionable.gd
+++ game/helpers/actionable.gd
@@ -3,12 +3,14 @@ extends Area2D
 
 const Balloon = preload("res://game/dialogue/balloon.tscn")
 
-
 @export var dialogue_resource: DialogueResource
 @export var dialogue_start: String = "start"
-
+var system_name: String = "[Actionable]"
 
 func action() -> void:
 	var balloon: Node = Balloon.instantiate()
 	get_tree().current_scene.add_child(balloon)
+	if dialogue_resource==null:
+		print(system_name,"Dialogue resource is null, cannot start dialogue.")
+		return
 	balloon.start(dialogue_resource, dialogue_start)
